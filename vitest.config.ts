9a744ce8b71c06e53d65b2d2f67473560330da7a
Andi-IM <andi.irhamm@gmail.com>
2026-01-01 21:07:32 +0700

    feat: add Vue plugins and path alias directly to Vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,26 +1,30 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { defineConfig, configDefaults } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**', 'src/main.ts'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      coverage: {
-        provider: 'v8',
-        reporter: ['text', 'json', 'html'],
-        thresholds: {
-          global: {
-            branches: 99,
-            functions: 99,
-            lines: 99,
-            statements: 99,
-          },
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**', 'src/main.ts'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        global: {
+          branches: 99,
+          functions: 99,
+          lines: 99,
+          statements: 99,
         },
       },
     },
-  }),
-)
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
